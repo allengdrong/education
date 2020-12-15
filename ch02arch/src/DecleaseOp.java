27a26f470f7b0ef92236c24fe436f84d40f64e6a
'allengdrong' <'bluesky0232@naver.com'>
2020-12-15 23:54:40 +0900

    20.12.15 study

--- ch02arch/src/DecleaseOp.java
+++ ch02arch/src/DecleaseOp.java
@@ -9,19 +9,14 @@ public class DecleaseOp {
 
 		System.out.println(i);
 		// i를 출력한다. // 19
-
 		System.out.println(--i);
 		// i를 1감소 시킨후 출력하세요를 한줄 명령어 // 18
-
 		System.out.println(i);
 		// i를 출력하세요. // 18
-
 		i--;
 		// i를 1감소 시키세요.
-
 		System.out.println(i);
 		// i를 출력하세요. // 17
-
 		System.out.println(i--);
 		// i를 먼저 출력하고 후 1감소 시키세요. 한줄 명령어 // 17
 
